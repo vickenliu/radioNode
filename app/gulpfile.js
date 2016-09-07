2b6fce7e44f2b41883731ee647be5c4109e64c85
Vicken <vicken.liu@gmail.com>
2016-09-07 23:17:43 +1200

    set up project structure and gulpfile

---
+++ app/gulpfile.js
@@ -0,0 +1,52 @@
+// Include Gulp
+var gulp = require('gulp');
+
+// Include plugins
+var plugins = require("gulp-load-plugins")({
+	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+	replaceString: /\bgulp[\-.]/
+});
+
+// Define default destination folder
+var dest = '../public/';
+
+
+/** tasks **/
+
+gulp.task('js', function() {
+
+	var jsFiles = ['src/js/**/*.js'];
+
+	  gulp.src(plugins.mainBowerFiles().concat(jsFiles))
+    .pipe(plugins.filter('**/*.js'))
+		.pipe(plugins.concat('main.js'))
+		.pipe(plugins.uglify())
+		.pipe(gulp.dest(dest + 'js/'));
+
+});
+
+gulp.task('css', function() {
+  var cssFiles = ['src/scss/*.scss']
+
+      gulp.src(plugins.mainBowerFiles().concat(cssFiles))
+  		.pipe(plugins.filter('**/*.*css'))
+      .pipe(plugins.order([
+              'normalize.css',
+              '*'
+            ]))
+      .pipe(plugins.sass())
+  		.pipe(plugins.concat('main.css'))
+      .pipe(plugins.autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+  		.pipe(gulp.dest(dest + 'css/'));
+
+});
+
+gulp.task('watch',function(){
+  gulp.watch('src/scss/*.scss',['css'])
+  gulp.watch('src/js/**/*.js',['js'])
+})
+
+gulp.task('default', ['css','js'])
