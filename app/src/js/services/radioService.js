8eecc31c05e75c94f9053f86ee62f18ac25de92e
Vicken <vicken.liu@gmail.com>
2016-09-08 23:40:20 +1200

    add function to get streaming url by station id

--- app/src/js/services/radioService.js
+++ app/src/js/services/radioService.js
@@ -1,9 +1,11 @@
 
 
 radioApp.service('radioService', ($http,$q) => {
-  const url = '/proxy?url='+'http://api.shoutcast.com/legacy/Top500?k=QVmydVad1rTssRzC&limit=5';
+
   return {
     getTop20Stations : () => {
+      const url = '/proxy?url='+'http://api.shoutcast.com/legacy/Top500?k=QVmydVad1rTssRzC&limit=5';
+
       return $http({url}).then((response)=>{
         const stations = $(response.data).find('station').map((i,station)=>{
           return {
@@ -15,8 +17,10 @@ radioApp.service('radioService', ($http,$q) => {
         return stations;
       })
    },
+
    getStreamUrl : (id) => {
      const url = '/proxy?url=http://yp.shoutcast.com/sbin/tunein-station.m3u?id='+id;
+
      return $http( { url } );
    }
 
