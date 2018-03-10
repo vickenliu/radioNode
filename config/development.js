a0e146018177f523ec2e375b06099aba8119f4aa
Vicken <vicken.liu@gmail.com>
2018-03-10 16:47:11 +1300

    add mongo auth

--- config/development.js
+++ config/development.js
@@ -2,5 +2,9 @@
 
 module.exports = {
     port: 3000,
-    mongodb: 'mongodb://localhost:27017'
+    mongodb: {
+        url: 'mongodb://localhost:27017',
+        username: 'webApp',
+        password: 'mypassword'
+    }
 }
--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,10 @@ let config = require('./')();
 let mongoose = require('mongoose');
 
 module.exports = () => {
-    var db = mongoose.connect(config.mongodb)
+    var db = mongoose.connect(config.mongodb.url, {
+        user: config.mongodb.username, 
+        pass: config.mongodb.password
+    });
     require('../models/radio')
 
     return db;
